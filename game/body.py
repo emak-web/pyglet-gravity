fcc207e7a67963d19fd7f302ac34c9aeb64b574c
Egor <Apex5802019october@gmail.com>
2025-04-03 22:24:43 +0100

    Controls fix

--- game/body.py
+++ game/body.py
@@ -7,8 +7,17 @@ class Body:
         self.shape = shapes.Circle(x=x, y=y, radius=radius, color=color)
         self.mass = mass
         self.velocity = velocity
+        self.velocity_vector = shapes.Line(0, 0, 0, 0, 2, (0, 0, 255))
         self.acceleration = Vector()
     
+    def draw(self):
+        self.shape.draw()
+    
+    def draw_velocity_vector(self):
+        self.velocity_vector.x, self.velocity_vector.y = self.shape.x, self.shape.y
+        self.velocity_vector.x2, self.velocity_vector.y2 = self.shape.x+self.velocity.x*10, self.shape.y+self.velocity.y*10
+        self.velocity_vector.draw()
+
     def move(self):
         self.velocity += self.acceleration
         self.shape.x += self.velocity.x
--- game/gravity.py
+++ game/gravity.py
@@ -1,5 +1,5 @@
-from pyglet import window, clock
-from pyglet.window import key
+from pyglet import window, clock, shapes
+from pyglet.window import key, mouse
 
 from game.vector import Vector
 from game.body import Body
@@ -11,12 +11,14 @@ class GravityWindow(window.Window):
         super(GravityWindow, self).__init__(1400, 1000, *args, **kwargs)
         self.set_location(850, 300)
 
+        self.keys = key.KeyStateHandler()
+        self.push_handlers(self.keys)      
+
         self.bodies = [
-            Body(700, 500, 10000, velocity=Vector(0, 0), radius=15),
-            Body(900, 500, 100, velocity=Vector(0, 5)),
-            Body(500, 500, 100, velocity=Vector(0, -4)),
+            Body(700, 500, 10000, velocity=Vector(0, 0), radius=15)
         ]
         self.selected = None
+        self.selected_vector_line = shapes.Line(0, 0, 0, 0, 2, (0, 0, 255))
         self.freeze = True
 
         clock.schedule_interval(self.update_game, 1/60)
@@ -34,8 +36,14 @@ class GravityWindow(window.Window):
 
     def on_draw(self):
         self.clear()
+        
         for b in self.bodies:
             b.shape.draw()
+            if self.freeze:
+                b.draw_velocity_vector()
+        
+        if self.selected and self.keys[key.V]:
+            self.selected_vector_line.draw()
     
     def update_game(self, dt):
         if not self.freeze:
@@ -50,26 +58,23 @@ class GravityWindow(window.Window):
             for b in self.bodies:
                 b.move()
 
-    def on_mouse_press(self, x, y, button, modifiers):
-        if self.selected:
-            self.selected.unselect()
-            self.selected = None
-
-        if modifiers & key.MOD_ACCEL:
-            self.bodies.append(Body(x, y, 100, 5, velocity=Vector(0, 5)))
-        else:
-            for b in self.bodies:
-                if (x, y) in b.shape:
-                    b.select()
-                    self.selected = b
-
     def on_key_press(self, symbol, modifiers):
         if modifiers & key.MOD_ACCEL:
             cursor = self.get_system_mouse_cursor(self.CURSOR_CROSSHAIR)
             self.set_mouse_cursor(cursor)
+        
+        elif symbol == key.ENTER and self.selected:
+            self.selected.velocity = Vector(
+                (self._mouse_x - self.selected.shape.x)/10, 
+                (self._mouse_y - self.selected.shape.y)/10
+            )
 
         elif symbol == key.SPACE:
             self.freeze = not self.freeze
+
+            if self.selected:
+                self.selected.unselect()
+                self.selected = None
 
         elif symbol == key.EQUAL and self.selected:
             self.selected.mass += 1000
@@ -78,6 +83,10 @@ class GravityWindow(window.Window):
         elif symbol == key.MINUS and self.selected:
             self.selected.mass -= 1000
             self.selected.shape.radius -= 1
+        
+        elif symbol == key.BACKSPACE and self.selected:
+            self.bodies.remove(self.selected)
+            self.selected = None
 
     def on_key_release(self, symbol, modifiers):
         if modifiers & key.MOD_ACCEL or key.UP:
@@ -87,4 +96,32 @@ class GravityWindow(window.Window):
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
         if self.selected:
             self.selected.shape.x, self.selected.shape.y = x, y
+    
+    def on_mouse_motion(self, x, y, dx, dy):
+        if self.selected:
+            self.selected_vector_line.x, self.selected_vector_line.y = self.selected.shape.x, self.selected.shape.y
+            self.selected_vector_line.x2, self.selected_vector_line.y2 = x, y
+    
+    def on_mouse_press(self, x, y, button, modifiers):
+        # Set velocity/unselect         
+        if self.selected:
+            if self.keys[key.V]:
+                self.selected.velocity = Vector(
+                    (x - self.selected.shape.x)/10, 
+                    (y - self.selected.shape.y)/10
+                )
+
+            self.selected.unselect()
+            self.selected = None
+            
+        # Select body 
+        if self.freeze:
+            for b in self.bodies:
+                if (x, y) in b.shape:
+                    b.select()
+                    self.selected = b
+
+        # Add body
+        if modifiers & key.MOD_ACCEL:
+            self.bodies.append(Body(x, y, 1000, 10))
             
